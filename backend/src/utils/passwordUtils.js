d3322aae1d439efd854b6b5ce834d8aecaba8847
oyen-bright <oyenbrihight@gmail.com>
2025-03-09 18:56:04 +0100

    Remove unused Android files and configurations; add backend password utility functions and .gitignore for backend

---
+++ backend/src/utils/passwordUtils.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword);
+};
+
+module.exports = {
+    hashPassword,
+    comparePassword
+};
